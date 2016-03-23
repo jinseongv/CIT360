3f0cd52f35decc3cc267ff3b1b81933b84dc4192
jinseongv <jinseongv@gmail.com>
2016-03-22 22:05:52 -0600

    exmaple of suit

---
+++ suit.java
@@ -0,0 +1,25 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//JUnit Suite Test
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ 
+   TestJunit1.class ,TestJunit2.class
+})
+public class JunitTestSuite {
+}
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+
+public class TestJunit1 {
+
+   String message = "Robert";	
+   MessageUtil messageUtil = new MessageUtil(message);
+   
+   @Test
+   public void testPrintMessage() {	
+      System.out.println("Inside testPrintMessage()");    
+      assertEquals(message, messageUtil.printMessage());     
+   }
+}
